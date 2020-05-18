64d5a52229a3201621f896b6b28d662944f65841
ExtraHash <ExtraHash@protonmail.com>
2020-05-17 21:43:01 -0600

    works pretty good now

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,39 +1,78 @@
 // tslint:disable: variable-name
 import chalk from 'chalk';
+import { Message, MessageAttachment } from 'discord.js';
 import log from 'electron-log';
-import { EventEmitter } from 'events';
 import knex, { Transaction } from 'knex';
 
-// tslint:disable-next-line: interface-over-type-literal
-type LinkObj = {
-  url: string;
-  author: string;
-  timestamp: number;
-  discord_id: string;
-};
-
-export class Database extends EventEmitter {
+export class Database {
   public ready: boolean;
   public sql: knex<any, unknown> = knex({
     client: 'sqlite3',
     connection: {
-      filename: './db.sqlite',
+      filename: './db2.sqlite',
     },
     useNullAsDefault: true,
   });
 
   constructor() {
-    super();
     this.ready = false;
     this.init();
   }
 
-  public async storeLink(linkObj: LinkObj): Promise<void> {
+  public async storeAttachment(
+    attachment: MessageAttachment,
+    post_id: string,
+    trx: any
+  ) {
+    const data = {
+      attachment: attachment.attachment,
+      height: attachment.height,
+      id: attachment.id,
+      name: attachment.name,
+      post_id,
+      proxy_url: attachment.proxyURL,
+      size: attachment.size,
+      url: attachment.url,
+      width: attachment.width,
+    };
+
+    await this.sql('attachments')
+      .insert(data)
+      .transacting(trx);
+  }
+
+  public async storeMessage(msg: Message, trx: any): Promise<void> {
+    // interesting things: attachments
+    const data = {
+      attachments: false,
+      author_avatar: msg.author.avatar,
+      author_id: msg.author.id,
+      author_username: msg.author.username,
+      comment: msg.cleanContent,
+      discord_id: msg.id,
+      pinned: msg.pinned,
+      timestamp: msg.createdTimestamp,
+    };
+    // log.debug(data);
+
+    const attachmentList = [...msg.attachments.values()];
+
+    if (attachmentList.length > 0) {
+      data.attachments = true;
+      for (const attachment of attachmentList) {
+        await this.storeAttachment(attachment, msg.id, trx);
+      }
+    }
+
     try {
-      await this.sql('links').insert(linkObj);
+      await this.sql('posts')
+        .insert(data)
+        .transacting(trx);
     } catch (err) {
       if (err.errno !== 19) {
         throw err;
+      } else {
+        log.warn('Duplicate entry! Shit!');
       }
     }
   }
@@ -43,8 +82,10 @@ export class Database extends EventEmitter {
     return query[0].topMessage;
   }
 
-  public async setTopMessage(discordID: string) {
-    await this.sql('internal').update({ topMessage: discordID });
+  public async setTopMessage(discordID: string, trx: any) {
+    await this.sql('internal')
+      .update({ topMessage: discordID })
+      .transacting(trx);
   }
 
   private async init(): Promise<void> {
@@ -55,15 +96,34 @@ export class Database extends EventEmitter {
     );
     const tableNames = tables.map((table: any) => table.name);
 
-    if (!tableNames.includes('links')) {
+    if (!tableNames.includes('posts')) {
       await this.sql.raw(
-        `CREATE TABLE "links" (
-          "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-          "url" TEXT,
-          "author" TEXT,
-          "timestamp" INTEGER,
+        `CREATE TABLE "posts" (
+          "author_avatar" TEXT,
+          "author_id" TEXT,
+          "author_username" TEXT,
+          "comment" TEXT,
           "discord_id" TEXT UNIQUE,
-          "submitted" BOOLEAN DEFAULT false
+          "pinned" BOOLEAN,
+          "timestamp" INTEGER,
+          "attachments" BOOLEAN
+        );`
+      );
+    }
+
+    if (!tableNames.includes('attachments')) {
+      await this.sql.raw(
+        `CREATE TABLE "attachments" (
+          "attachment" TEXT,
+          "name" TEXT,
+          "id" TEXT,
+          "size" INTEGER,
+          "url" TEXT,
+          "proxy_url" TEXT,
+          "height" INTEGER,
+          "width" INTEGER,
+          "post_id" TEXT,
+          "buffer" BLOB
         );`
       );
     }
@@ -74,7 +134,6 @@ export class Database extends EventEmitter {
           "topMessage" TEXT
         );`
       );
-
       await this.sql('internal').insert({});
     }
 
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { performance } from 'perf_hooks';
 import { CHANNEL_ID, GENESIS_MESSAGE_ID } from './constants/constants';
 import { Database } from './db/db';
 import { loadEnv } from './utils/loadEnv';
+import { sleep } from './utils/sleep';
 
 // load the environment variables
 loadEnv();
@@ -23,14 +24,24 @@ async function main() {
 async function scrape() {
   const client = new Client();
   client.on('ready', async () => {
-    const startTime = performance.now();
+    let synced = false;
+    let startTime = performance.now();
     log.info(`Logged in as ${client.user!.tag}!`);
-    const otChannel: Channel = await client.channels.fetch(CHANNEL_ID);
-    const messageManager = (otChannel as TextChannel).messages;
+    const scrapedChannel: TextChannel = (await client.channels.fetch(
+      CHANNEL_ID
+    )) as TextChannel;
+    const messageManager = scrapedChannel.messages;
     let topMessage = (await db.getTopMessage()) || GENESIS_MESSAGE_ID;
     let messagesScraped = 0;
 
     while (true) {
+      if (synced) {
+        startTime = performance.now();
+      }
+
+      log.debug('Current top message is ' + topMessage);
+      log.debug(`Scraped ${messagesScraped.toString()} so far.`);
+
       const messages: any = await messageManager.fetch({
         after: topMessage,
         limit: 50,
@@ -39,42 +50,32 @@ async function scrape() {
       const msgList = [...messages.values()].reverse();
 
       if (msgList.length === 0) {
-        break;
+        const endTime = performance.now();
+        log.info(
+          'Scraped ' +
+            messagesScraped.toString() +
+            ' messages in ' +
+            ((endTime - startTime) / 1000).toFixed(2) +
+            ' seconds'
+        );
+        synced = true;
+        messagesScraped = 0;
+        await sleep(10000);
       }
 
-      msgList.map(async (msg: Message) => {
-        const { content } = msg;
-
-        const urlMatches = content.match(/\bhttps?:\/\/\S+/gi);
-
-        if (urlMatches) {
-          urlMatches.map(async (url) => {
-            const linkObj = {
-              author: msg.author.username,
-              discord_id: msg.id,
-              timestamp: msg.createdTimestamp,
-              url,
-            };
-            await db.storeLink(linkObj);
-            console.log(linkObj);
-          });
-        }
-
-        if (msgList.indexOf(msg) === msgList.length - 1) {
+      await db.sql.transaction(async (trx) => {
+        for (const msg of msgList) {
+          await db.storeMessage(msg, trx);
           topMessage = msg.id;
-          await db.setTopMessage(msg.id);
+          if (msgList.indexOf(msg) === msgList.length - 1) {
+            await db.setTopMessage(msg.id, trx);
+          }
         }
       });
+
       messagesScraped += msgList.length;
+      continue;
     }
-    const endTime = performance.now();
-    log.info(
-      'Scraped ' +
-        messagesScraped.toString() +
-        ' messages in ' +
-        ((endTime - startTime) / 1000).toFixed(2) +
-        ' seconds'
-    );
   });
 
   client.login(process.env.DISCORD_TOKEN);
